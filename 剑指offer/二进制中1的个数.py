deb02b59b142284e43c5ac4c869be1ca4b8c59f6
WuLC <liangchaowu5@gmail.com>
2017-04-23 19:41:22 +0800

    Bit manipulation

---
+++ 剑指offer/二进制中1的个数.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author: WuLC
+# @Date:   2017-04-23 19:40:23
+# @Last modified by:   WuLC
+# @Last Modified time: 2017-04-23 19:40:46
+# @Email: liangchaowu5@gmail.com
+
+
+"""
+输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+"""
+
+# bit manipulation
+class Solution:
+    def NumberOf1(self, n):
+        result = 0
+        for _ in xrange(32):
+            result += (1&n)
+            n >>= 1
+        return result
