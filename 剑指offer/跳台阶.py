19acf944e1a06d22624f609d990610bf1223f84d
WuLC <liangchaowu5@gmail.com>
2017-04-23 19:35:38 +0800

    Dp, time complexity: O(n); similar to Fibonaci Array

--- 剑指offer/跳台阶.py
+++ 剑指offer/跳台阶.py
@@ -2,8 +2,14 @@
 # @Author: WuLC
 # @Date:   2017-04-23 16:32:48
 # @Last modified by:   WuLC
-# @Last Modified time: 2017-04-23 16:34:17
+# @Last Modified time: 2017-04-23 19:35:26
 # @Email: liangchaowu5@gmail.com
+
+
+
+"""
+一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+"""
 
 # dp, time complexity: O(n)
 # similar to Fibonaci Array
