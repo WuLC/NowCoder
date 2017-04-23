58e3c15986947247e86b047d1940c4d6c0ae4c88
WuLC <liangchaowu5@gmail.com>
2017-04-23 17:00:14 +0800

    数学归纳法，2^(n-1)

--- 剑指offer/变态跳台阶.py
+++ 剑指offer/变态跳台阶.py
@@ -2,11 +2,11 @@
 # @Author: WuLC
 # @Date:   2017-04-23 16:59:16
 # @Last modified by:   WuLC
-# @Last Modified time: 2017-04-23 16:59:33
+# @Last Modified time: 2017-04-23 17:00:08
 # @Email: liangchaowu5@gmail.com
 
 
-# 数学归纳法
+# 数学归纳法，2^(n-1)
 class Solution:
     def jumpFloorII(self, number):
         return pow(2, number-1)
