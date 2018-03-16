d7defe267aff9d0c4ae7fe638eeb88bd045fb69b
WuLC <liangchaowu5@gmail.com>
2018-03-16 20:07:39 +0800

    bit manipulation

--- 剑指offer/不用加减乘除做加法.py
+++ 剑指offer/不用加减乘除做加法.py
@@ -4,6 +4,24 @@
 # EMail: liangchaowu5@gmail.com
 
 # bit manipulation
+
+# Java version works
+public class Solution
+{
+    public int Add(int num1,int num2) 
+    {
+        int carry;
+        while(num2!=0)
+        {
+            carry = (num1&num2)<<1;
+            num1 = num1^num2;
+            num2 = carry;
+        }
+        return num1;
+    }
+}
+
+# python version do not work cause it will automatically convert int to long
 class Solution:
     def Add(self, num1, num2):
         while num2 != 0:
